15dcb40bec5ff6a776c6d79f32ed33c30d1128d4
michal <michal.drzewiecki@grandparade.co.uk>
2022-12-23 15:34:16 +0100

    feat: handle filter clear all

--- src/components/side-filters/components/filter-elements/checkbox-filter-element/checkbox-filter-element.tsx
+++ src/components/side-filters/components/filter-elements/checkbox-filter-element/checkbox-filter-element.tsx
@@ -2,15 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { FilterOperatorEnum } from '../../../../../enum';
 import { changeFilters, clearFilter } from '../../../../../store/redux/actions/filters';
+import { useSelector } from '../../../../../store/redux/useSelector';
 import { CheckboxFilterElementPropsInterface } from './checkbox-filter-element-props-interface';
 import './checkbox-filter-element.scss';
 
 export const CheckboxFilterElement = ({params: {options, filterElementName}}: CheckboxFilterElementPropsInterface) => {
   const [selectedValues, setSelectedValues] = useState<string[] | null>(null);
+  const [externalFilterChange, setExternalFilterChange] = useState<boolean>(false);
   const dispatch = useDispatch();
+  const filtersData = useSelector(state => state.filtersData.filters);
 
   useEffect(() => {
-    if (!selectedValues) {
+    if (!selectedValues || externalFilterChange) {
       return;
     }
     selectedValues.length ?
@@ -23,6 +26,7 @@ export const CheckboxFilterElement = ({params: {options, filterElementName}}: Ch
   }, [selectedValues]);
 
   const handleCheckBoxValueChanged = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setExternalFilterChange(false);
     const isChecked = event.target.checked;
     const value = event.target.value;
     if (isChecked) {
@@ -32,10 +36,39 @@ export const CheckboxFilterElement = ({params: {options, filterElementName}}: Ch
     }
   }
 
+  useEffect(() => {
+    const foundFilterData = filtersData.find(singleFilterData => singleFilterData.field === filterElementName);
+    if (!foundFilterData) {
+      if (selectedValues === null) {
+        return;
+      }
+      if (selectedValues.length) {
+        setExternalFilterChange(true);
+        setSelectedValues([]);
+      }
+    } else {
+      setExternalFilterChange(true);
+      if (!selectedValues) {
+        setSelectedValues(foundFilterData.values);
+      }
+      const areValuesDifferent = foundFilterData.values.some(value => !selectedValues?.includes(value));
+      if (areValuesDifferent) {
+        setSelectedValues(foundFilterData.values);
+      }
+    }
+
+  }, [filtersData]);
+
   return <div>
     {
       options.map(option => <div key={option.value} className={'checkboxElement'}>
-        <input type={'checkbox'} id={option.value} value={option.value} onChange={handleCheckBoxValueChanged}/>
+        <input
+          type={'checkbox'}
+          id={option.value}
+          value={option.value}
+          onChange={handleCheckBoxValueChanged}
+          checked={selectedValues?.includes(option.value)}
+        />
         <label htmlFor={option.value}>{option.displayName}</label>
       </div>)
     }
